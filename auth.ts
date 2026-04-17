83f1b9c8520f5cff0ff8c94c84df09cdffcbebe4
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-17 19:26:32 +0530

    update auth to include next cookies plugin to automatically set cookies for actions that require setting cookies on server

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { nextCookies } from 'better-auth/next-js'
+
 import prisma from '@/lib/prisma'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  plugins: [nextCookies()],
 })
