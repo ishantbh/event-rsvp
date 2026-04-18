5fec0d51c5ea07dc88d404f8745e2d63b358dcb1
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-18 18:05:01 +0530

    refine sign-up-form schema to be validate password and confirm password field equality

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -5,9 +5,16 @@ export const SignInFormSchema = z.object({
   password: z.string().min(1, 'Password is required'),
 })
 
-export const SignUpFormSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  email: z.email(),
-  password: z.string().min(6, 'Password must be at least 6 characters long'),
-  confirmPassword: z.string().min(1, 'Confirm password is required'),
-})
+export const SignUpFormSchema = z
+  .object({
+    name: z.string().min(1, 'Name is required'),
+    email: z.email(),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: z
+      .string()
+      .min(6, 'Confirm password must be at least 6 characters long'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  })
