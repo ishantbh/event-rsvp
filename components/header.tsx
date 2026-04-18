7a3efce78d68658c4bb139f894ed66d35c56d98f
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-18 20:28:16 +0530

    update header to be client component to update realtime when auth state changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,14 @@
-import Link from 'next/link'
-import { headers } from 'next/headers'
+'use client'
 
-import { auth } from '@/auth'
+import Link from 'next/link'
+
+import { authClient } from '@/lib/auth-client'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/theme/mode-toggle'
 import { UserAvatar } from '@/components/user-avatar'
 
-export async function Header() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
+export function Header() {
+  const { data: session, isPending, error, refetch } = authClient.useSession()
 
   return (
     <header className='border-b bg-background backdrop-blur'>
@@ -19,35 +18,37 @@ export async function Header() {
         </div>
 
         <div className='flex items-center gap-4'>
-          <nav>
-            <ul className='flex items-center gap-4'>
-              {session ? (
-                <>
+          {!isPending && !error && (
+            <nav>
+              <ul className='flex items-center gap-4'>
+                {session ? (
+                  <>
+                    <li>
+                      <Button
+                        variant='link'
+                        className='text-muted-foreground'
+                        asChild
+                      >
+                        <Link href='/dashboard'>Dashboard</Link>
+                      </Button>
+                    </li>
+
+                    <UserAvatar />
+                  </>
+                ) : (
                   <li>
                     <Button
                       variant='link'
                       className='text-muted-foreground'
                       asChild
                     >
-                      <Link href='/dashboard'>Dashboard</Link>
+                      <Link href='/sign-in'>Sign In</Link>
                     </Button>
                   </li>
-
-                  <UserAvatar />
-                </>
-              ) : (
-                <li>
-                  <Button
-                    variant='link'
-                    className='text-muted-foreground'
-                    asChild
-                  >
-                    <Link href='/sign-in'>Sign In</Link>
-                  </Button>
-                </li>
-              )}
-            </ul>
-          </nav>
+                )}
+              </ul>
+            </nav>
+          )}
 
           <ModeToggle />
         </div>
