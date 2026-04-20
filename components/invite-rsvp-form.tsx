a8874279b7ef07b796daae1faee85689548a44c0
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-20 21:46:49 +0530

    add invite-rsvp form using tanstack-form

--- components/invite-rsvp-form.tsx
+++ components/invite-rsvp-form.tsx
@@ -1,5 +1,144 @@
 'use client'
 
+import { useForm } from '@tanstack/react-form'
+
+import { RsvpStatus as PrismaRsvpStatus } from '@/lib/generated/prisma/enums'
+import { InviteRsvpFormSchema } from '@/lib/schemas'
+import { Button } from '@/components/ui/button'
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
 export function InviteRsvpForm() {
-  return <div>RSVP Form</div>
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      attendance: '',
+    },
+    validators: {
+      onSubmit: InviteRsvpFormSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value)
+    },
+  })
+
+  return (
+    <form
+      id='invite-rsvp-form'
+      onSubmit={(e) => {
+        e.preventDefault()
+        form.handleSubmit()
+      }}
+    >
+      <FieldGroup>
+        <form.Field
+          name='name'
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  aria-invalid={isInvalid}
+                  placeholder='John Doe'
+                  required
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+
+        <form.Field
+          name='email'
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <Input
+                  type='email'
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  aria-invalid={isInvalid}
+                  placeholder='john@example.com'
+                  required
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+
+        <form.Field
+          name='attendance'
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Attendance</FieldLabel>
+                <Select
+                  name={field.name}
+                  value={field.state.value}
+                  onValueChange={field.handleChange}
+                >
+                  <SelectTrigger
+                    id='invite-rsvp-form-attendance'
+                    aria-invalid={isInvalid}
+                    className='min-w-30'
+                  >
+                    <SelectValue placeholder='Select' />
+                  </SelectTrigger>
+
+                  <SelectContent position='item-aligned'>
+                    <SelectItem value={PrismaRsvpStatus.going}>
+                      Going
+                    </SelectItem>
+                    <SelectItem value={PrismaRsvpStatus.maybe}>
+                      Maybe
+                    </SelectItem>
+                    <SelectItem value={PrismaRsvpStatus.not_going}>
+                      Not Going
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+
+        <Field orientation='horizontal'>
+          <Button type='button' variant='outline' onClick={() => form.reset()}>
+            Reset
+          </Button>
+          <Button type='submit'>Submit</Button>
+        </Field>
+      </FieldGroup>
+    </form>
+  )
 }
