386dff9457ce8d4ca3bdcc85d73a7b3611164470
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-25 16:37:21 +0530

    improve no-events component card size by adding a max-width

--- components/no-events.tsx
+++ components/no-events.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 export function NoEvents({ query }: { query?: string }) {
   return (
     <div className='flex-1 flex items-center justify-center'>
-      <Card>
+      <Card className='w-full max-w-sm mx-auto text-center'>
         <CardHeader>
           <CardTitle>No events found</CardTitle>
         </CardHeader>
